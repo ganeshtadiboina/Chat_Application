6573dca3a1ac7bb25d52cb312b01a39796b4617e
Ganesh Tadiboina <ganeshtadiboina3127@gmail.com>
2025-05-21 08:37:54 +0530

    Initial file transfer

---
+++ client/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from '../context/AuthContext.jsx'
+// import { ChatProvider } from '../context/ChatContainer.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter future={{ 
+    v7_relativeSplatPath: true,
+    v7_startTransition: true 
+  }}>
+    <AuthProvider>
+      {/* <ChatProvider> */}
+        <App />
+      {/* </ChatProvider> */}
+    </AuthProvider>
+  </BrowserRouter>,
+)
---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' ws: wss: http: https:;"
+    }
+  }
+})
